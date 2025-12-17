34429bea49e830185fd01bfc335996337051f7d5
bala-amypo <bala@amypo.in>
2025-12-17 10:00:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/studentController.java
+++ src/main/java/com/example/demo/controller/studentController.java
@@ -1,12 +1,31 @@
+// package com.example.demo.controller;
+// import org.springframwork.web.bind
+// import com.example.demo.entity.";
+
+// @RestController
+// public class studentController {
+
+// @GetMapping("/getAllStudent")l
+// public List<studentEntity> getAll(){
+
+//    }
+
+// }
+
 package com.example.demo.controller;
 import org.springframwork.web.bind
-import com.example.demo.entity.";
+import com.example.demo.entity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController; 
+import java.util.List;
+
 
 @RestController
 public class studentController {
 
 @GetMapping("/getAllStudent")l
 public List<studentEntity> getAll(){
+
 
    }
 
--- src/main/java/com/example/demo/repository/repo.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.repository;
-
-public interface studentRepo extends JpaRepository{
-
-}
