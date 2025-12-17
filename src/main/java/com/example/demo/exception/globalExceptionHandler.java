8972b65801d27fccae53fab61ae3ff34c19739bb
bala-amypo <bala@amypo.in>
2025-12-17 10:35:26 +0000

    User commit

--- src/main/java/com/example/demo/exception/globalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/globalExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.example.demo.exception;
 
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import java.util.*;
 
 @RestControllerAdvice
 public class globalExceptionHandler{
     
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-         
-    }
+    public ResponseEntity<?> handleFieldError(MethodArgumentNotValidException){
+        Map<String,String> error = new HashMap<>();
+        ex.getBindingData.getFieldsError().forEach(err- > error.put(ex.getField(),ex.get))
+     }
+ }
