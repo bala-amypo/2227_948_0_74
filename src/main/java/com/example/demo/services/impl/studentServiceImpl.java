7e968c5626696bea204ec7da76bbcc4d6459a185
bala-amypo <bala@amypo.in>
2025-12-18 00:06:49 +0000

    User commit

--- src/main/java/com/example/demo/services/impl/studentServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/studentServiceImpl.java
@@ -25,44 +25,44 @@ public class studentServiceImpl implements studentService {
         return repo.save(student);
     }
 
-//     @Override
-//     public studentEntity addStudent(studentEntity stu) {
-//    return repo.save(stu);
-//     }
+    @Override
+    public studentEntity addStudent(studentEntity stu) {
+   return repo.save(stu);
+    }
 
-//    @Override
-//    public studentEntity addStudent(studentEntity stu) {
-//    return repo.save(stu);
-//    }
+   @Override
+   public studentEntity addStudent(studentEntity stu) {
+   return repo.save(stu);
+   }
 
-//    @Override
-//    public studentEntity getById(@PathVariable Long id){
-//     Optional<studentEntity> data = repo.findById(id);
-//        if(data.isPresent()){
-//          return data.get();
-//        }else{
-//             return null;
-//              }
-//             }
+   @Override
+   public studentEntity getById(@PathVariable Long id){
+    Optional<studentEntity> data = repo.findById(id);
+       if(data.isPresent()){
+         return data.get();
+       }else{
+            return null;
+             }
+            }
 
-//    @Override
-//    public String updateStudent(Long id, studentEntity newstu) {
-//    if(repo.existsById(id)){
-//    newstu.setId(id);
-//          repo.save(newstu);
-//          return "Student updated";
-//    }
+   @Override
+   public String updateStudent(Long id, studentEntity newstu) {
+   if(repo.existsById(id)){
+   newstu.setId(id);
+         repo.save(newstu);
+         return "Student updated";
+   }
   
-//    return "Student not found"
-// } 
+   return "Student not found"
+} 
 
-//    @Override
-//    public String deleteStudent(long id){
-//     if(repo.existsById(id)){
-//         repo.deleteById(id);
-//         return "Student deleted";
-//     }
+   @Override
+   public String deleteStudent(long id){
+    if(repo.existsById(id)){
+        repo.deleteById(id);
+        return "Student deleted";
+    }
    
-//     return "Student not found";
-// }
+    return "Student not found";
+}
 }
