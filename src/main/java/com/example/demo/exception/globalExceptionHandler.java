83f55827fdb0c9fef45d6438e07c456c713fbedb
bala-amypo <bala@amypo.in>
2025-12-18 08:21:47 +0000

    User commit

--- src/main/java/com/example/demo/exception/globalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/globalExceptionHandler.java
@@ -26,6 +26,12 @@ public class globalExceptionHandler {
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<String> handleStudentNotValid(StudentNotFoundException ex){
+           return ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+    } 
+    
 }
 
 
